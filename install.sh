1867294e598bec2b75875bf60917fadacaded652
Filipe Costa <filipebarcos@gmail.com>
2012-08-17 13:19:53 -0300

    Added bash_profile; started to organize aliases.

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+ln -s ~/workspace/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/workspace/dotfiles/aliases ~/.aliases
 ln -s ~/workspace/dotfiles/zshrc ~/.zshrc
 ln -s ~/workspace/dotfiles/tmux.conf ~/.tmux.conf
