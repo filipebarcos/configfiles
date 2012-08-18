c2567e47cbecd8dac44f5a57e5235fb49fd25c51
Filipe Costa <filipebarcos@gmail.com>
2012-08-18 10:43:33 -0300

    removed install file to use only Rakefile

--- install.sh
+++
@@ -1,5 +0,0 @@
-ln -s ~/workspace/dotfiles/bash_profile ~/.bash_profile
-ln -s ~/workspace/dotfiles/aliases ~/.aliases
-ln -s ~/workspace/dotfiles/zshrc ~/.zshrc
-ln -s ~/workspace/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/workspace/dotfiles/gitconfig ~/.gitconfig
